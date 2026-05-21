7bd0475e4b9a8f845650259796a06bd71e368850
MuhammedIhsan S EXT <muhammedihsan.s@in.abb.com>
2026-05-21 06:39:34 +0000

    Added sys/inotify.h include file for linux platform

--- plugins/crypto/ua_certificategroup_filestore.c
+++ plugins/crypto/ua_certificategroup_filestore.c
@@ -23,6 +23,7 @@
 #if defined(__linux__) || defined(UA_ARCHITECTURE_WIN32) || defined(__APPLE__) || defined(UA_ARCHITECTURE_OUL)
 
 #ifdef __linux__
+#include <sys/inotify.h>
 #define EVENT_SIZE (sizeof(struct inotify_event))
 #define BUF_LEN (1024 * ( EVENT_SIZE + 16 ))
 #endif /* __linux__ */
